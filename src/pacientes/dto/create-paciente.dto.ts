93bad282e19e087fcf7f87b9f0ef722ac82cb914
njorquera98 <njorquera98.dev@gmail.com>
2024-05-14 00:54:35 -0400

    dto paciente

--- src/pacientes/dto/create-paciente.dto.ts
+++ src/pacientes/dto/create-paciente.dto.ts
@@ -1,7 +1,34 @@
-import { IsString } from "class-validator";
+import { IsDateString, IsNumber, IsString } from "class-validator";
 
 export class CreatePacienteDto {
 
   @IsString()
   nombre: string;
+
+  @IsString()
+  apellido: string;
+
+  @IsNumber()
+  telefono: number;
+
+  @IsString()
+  correo: string;
+
+  @IsString()
+  prevision: string;
+
+  @IsString()
+  evaluacion: string;
+
+  @IsString()
+  objetivo: string;
+
+  @IsString()
+  diagnostico: string;
+
+  @IsDateString()
+  fecha_nacimiento: Date;
+
+  @IsDateString()
+  fecha_ingreso: Date;
 }
